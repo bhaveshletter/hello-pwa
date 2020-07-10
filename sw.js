f137dcdebeb030b707d845416f6f3bf96539ce9e
bhaveshletter <bhaveshletter@gmail.com>
2020-07-10 16:54:45 +0530

    initial commit

---
+++ sw.js
@@ -0,0 +1,52 @@
+// version
+// files to cache
+var version = 'v0.01';
+var files = [
+    './',
+    './index.html',
+    '../assets/css/index.css',
+    '../assets/js/index.js',
+    '../assets/images/favicon.ico'
+  ];
+
+//  install
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(version)
+      .then(cache => {
+        return cache.addAll(files)
+        .catch(err => {
+          console.error('Error in adding files during install of SW.', err)
+        })
+      })
+  )
+  console.log('SW installed.')
+  self.skipWaiting();
+})
+
+//  activate
+self.addEventListener('activate', event =>{
+  event.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+            cacheNames.map(cache => {
+              if(cache !== version){
+                return caches.delete(cache)
+              }
+            })
+          )
+      })
+  )  
+  self.clients.claim();
+})
+
+//  fetch
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(res => {
+        return res || fetch(event.request);
+      })
+  )
+})
